cbaba34942bd700f40554e8d3862a8f49946dc13
Jacob Schenkelberg <jtschenk@ksu.edu>
2021-05-03 15:17:00 -0500

    Completed input_student

--- jtschenk_hw5.py
+++ jtschenk_hw5.py
@@ -1,13 +1,16 @@
 def input_student():
+    name_list = []
+    id_list = []
     is_new_student = True
     while(is_new_student == True):
         student_name = input("What is your name?: ")
+        name_list.append(student_name)
         student_id = input("\nWhat is your id number?: ")
+        id_list.append(student_id)
         enter_student = input("Enter another student? (y)es or (n)o: ")
         if(enter_student == "Y" or enter_student == "y"):
             pass
         elif(enter_student == "N" or enter_student == "n"):
-            print("leave")
             break
         else:
             while(enter_student != "Y" or enter_student != "y" or enter_student != "N" or enter_student != "n"):
@@ -18,10 +21,11 @@ def input_student():
                 elif(enter_student == "n" or enter_student == "N"):
                     is_new_student = False
                     break
+    return name_list, id_list
+
 
 def input_assignments():
     pass
-
 def grade_student():
     pass
 
@@ -32,3 +36,4 @@ def main():
     pass
 
 input_student()
+input_assignments()
